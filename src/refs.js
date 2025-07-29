7115036b2fa7b9bc75bcf0a54686599589621947
StringLOVERgirl <marchenkostar@gmail.com>
2025-07-29 15:02:42 +0300

    sleep

--- src/refs.js
+++ src/refs.js
@@ -3,29 +3,52 @@ import { useRef, useState,useEffect } from "react";
 export function Refs (){
 
     const observerRef = useRef(null)
+    const cont1Ref = useRef(null)
+    const cont2Ref = useRef(null)
+    const zeroRef = useRef(null)
     let [link1Hidden, setLink1Hidden] = useState(false)
 
     useEffect(()=>{
 
-        observerRef.current = new IntersectionObserver(() => {
-
-        },{
-            threshold: 0.5,
+        observerRef.current = new IntersectionObserver((massiv) => {
+            if(massiv[0].isIntersecting){
+                console.log('zero')
+             setLink1Hidden(true)
+            }  else {
+                setLink1Hidden(false)
+            }
+        }, {
+            threshold: 0.25,
             root: null
         })
-        observerRef.current.observe()
+
+        if (zeroRef.current){
+            console.log('zero')
+          observerRef.current.observe(zeroRef.current)
+        }
+
+        return(()=>{
+            observerRef.current.disconnect()
+        })
 
     },[])
 
 
     return(<>
         <section className="section_links">
-            <div className={`link_cont link1_cont ${link1Hidden ? 'hide_link1_cont' : ''}`}>
+            <div className={`link_cont link1_cont ${link1Hidden ? 'hide_link1_cont' : ''}`}
+            // ref={cont1Ref}
+            >
                 <div className="link link1"></div>
+                {/* <video className="gg" src='assets/63fca69523dfba725518d390_68399bcc53dcba2367185502_hero-4k-transcode.webm' autoPlay muted loop></video> */}
             </div>
-            <div className="link_cont link2_cont ">
+            <div className="link_cont link2_cont "
+            //    ref={cont2Ref}
+               >
                 <div className="link link2"></div>
             </div>
+            <div className="element_zero"
+            ref={zeroRef}></div>
         </section>
     </>)
 }

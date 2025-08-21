b895c63230f685dfeb1cc60078b4ac13918597fb
StringLOVERgirl <marchenkostar@gmail.com>
2025-08-22 02:39:10 +0300

    footer  mobile

--- src/refs.js
+++ src/refs.js
@@ -91,7 +91,7 @@ const bgRefs = useRef([])
 
 
     const parallax = (varbg,i) => {
-        const speed = 50
+        const speed = 30
         let value = (window.scrollY - parallaxMetrics.current.distance[i]) / parallaxMetrics.current.step - speed
         if (value > 0 ){ value = 0} 
         if (value < -30 ){ value = -30} 

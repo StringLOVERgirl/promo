1f43d9352184888b078836a22c9cbf521162c2ac
StringLOVERgirl <marchenkostar@gmail.com>
2025-08-23 04:12:17 +0300

    paralax

--- src/refs.js
+++ src/refs.js
@@ -92,7 +92,7 @@ const bgRefs = useRef([])
 
     const parallax = (varbg,i) => {
         const speed = 30
-        let value = ((window.scrollY - parallaxMetrics.current.distance[i]) / parallaxMetrics.current.step - speed)/2
+        let value = (window.scrollY - parallaxMetrics.current.distance[i]) / parallaxMetrics.current.step - speed
         if (value > 0 ){ value = 0} 
         if (value < -30 ){ value = -30} 
         // убираем выше нижний рвыок 
@@ -144,7 +144,7 @@ const flag = useRef(false)
                         && event.targetScroll <= parallaxMetrics.current.distance[i] + parallaxMetrics.current.scrollWay
                     ) {
                         requestAnimationFrame(() => {
-                            parallax('--bg' + (i + 1), i)
+                            // parallax('--bg' + (i + 1), i)
                             console.log('prarl')
                         })
                     } 

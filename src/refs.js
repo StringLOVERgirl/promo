cd7265f0c3ea81b5acbfa55a45410049ff533187
StringLOVERgirl <marchenkostar@gmail.com>
2025-08-23 19:19:39 +0300

    mobile

--- src/refs.js
+++ src/refs.js
@@ -92,11 +92,11 @@ const bgRefs = useRef([])
 
     const parallax = (varbg,i,event) => {
         const speed = 30
-        // let value = ((window.scrollY - parallaxMetrics.current.distance[i]) / parallaxMetrics.current.step - speed) 
-        let value = -(window.scrollY - parallaxMetrics.current.distance[i])/10
+        let value = ((window.scrollY - parallaxMetrics.current.distance[i]) / parallaxMetrics.current.step - speed) 
+        // let value = -(window.scrollY - parallaxMetrics.current.distance[i])/10
         console.log(value)
 
-        // if (value > 0 ){ value = 0} 
+        if (value > 0 ){ value = 0} 
         if (value < -30 ){ value = -30} 
         console.log(value)
         // убираем выше нижний рвыок 
